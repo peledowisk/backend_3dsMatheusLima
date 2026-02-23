439e4920027e2a82de0a0e7268896d7418aa385d
pelédowisk <matheus.rodrigues09@escola.pr.gov.br>
2026-02-23 12:35:58 +0000

    criar variaveis para , e produto

---
+++ variaveis/variavel-pessoa.js
@@ -0,0 +1,26 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9, 0,7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = 'petter' //string
+let sobrenome = 'parker' //string
+const cpf = "888888888-88" //string
+const rg = '101212 ssp' //string
+let idade = 22 //number
+let solteiro = true //boolean
+let nota = 7+2 // number
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
+
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof cpf)
+console.log(typeof rg)
+console.log(typeof idade)
+console.log(typeof solteiro)
+console.log(typeof nota)
---
+++ variaveis/variavel-produto.js
@@ -0,0 +1,23 @@
+/**
+ * CRIE 5 VARIÁVEIS PARA CARACTERÍSTICAS DE UM PRODUTO DO MERCADO LIVRE
+ * COSIDERE SE A VARIÁVEL , É let ou const
+ * IMPRIMA COM console.log(<variável>);
+ * IMPRIMA COM console.log("NOME-VARIÁVEL: " + <variável>)
+ */
+let nomeProduto = 'Notebook Gamer' //string
+let preco = 5000.00 //number
+const marca = 'Dell' //string
+let estoque = 10 //number
+const categoria = 'Eletrônicos' //string
+
+console.log(nomeProduto)
+console.log(preco)
+console.log(marca)
+console.log(estoque)
+console.log(categoria)
+
+console.log("NOME-PRODUTO: " + nomeProduto)
+console.log("PREÇO: " + preco)
+console.log("MARCA: " + marca)
+console.log("ESTOQUE: " + estoque)
+console.log("CATEGORIA: " + categoria)
