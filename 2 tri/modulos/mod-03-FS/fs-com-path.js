37c045695a9e5cb663c912080329f066d23d6a13
pelédowisk <matheus.rodrigues09@escola.pr.gov.br>
2026-06-03 12:31:20 +0000

    alteração FS com path erro

--- 2 tri/modulos/mod-03-FS/fs-com-path.js
+++ 2 tri/modulos/mod-03-FS/fs-com-path.js
@@ -1,12 +1,21 @@
 // importar PATH e FS
 // Path possui (join para juntar caminhos e Resolve
 // para deduzir subdiretório e S.O.)
+const { error } = require('console')
 const fs = require('fs')
 const path = require ('path')
 
 // Salvar o Caminho em um variável
 //  const caminho = path.join(__dirname,'README.md') //   Markdown,text,doc
-const caminho = path.resolve(__dirname,'README.md')//  Abrir, ler, deletar, editar
+try {
+const caminho = path.resolve(__dirname,'REAdME.md')//  Abrir, ler, deletar, editar
 const arquivo = fs.readFileSync(caminho,'utf-8')
 
 console.log(arquivo)
+}catch(erro){
+console.log('-------------------------')
+console.warn("Atenção!"+erro.path)
+throw new error('Mensagem de erro personalizada');
+
+console.log('-------------------------')
+}
